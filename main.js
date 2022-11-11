63f4a9c29fd3230181359eec455b0375aefc535b
肥田 幸久(hz190162wz) <hz190162wz@jindai.jp>
2022-11-11 21:59:55 +0900

    a

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const MATH_NUM = 8;
 const TABLE = document.getElementById("table");
 
 let nowColor = 1;
-const COLORS = ["white", "black"];
+const COLORS = ["white", "green", "black"];
 
 const ARRAY = [];
 for (let row = 0; row < MATH_NUM; row++) {
@@ -13,43 +13,65 @@ for (let row = 0; row < MATH_NUM; row++) {
         const TD = document.createElement("td");
         TD.classList.add("block");
         TR.appendChild(TD);
-        ARRAY[row][col] = { num: undefined, elm: TD, stone: undefined };
+        ARRAY[row][col] = { num: 0, elm: TD, stone: document.createElement("div") };
+        ARRAY[row][col].stone.classList.add("stone");
+        reflecColor(ARRAY[row][col]);
+        TD.appendChild(ARRAY[row][col].stone);
         TD.addEventListener("click", () => {
             console.log(col, row);
             if (!ARRAY[row][col].num) {
                 ARRAY[row][col].num = nowColor;
+                reflecColor(ARRAY[row][col]);
                 nowColor *= -1;
-                const STONE = document.createElement("div");
-                STONE.classList.add("stone");
-                STONE.style.background = COLORS[(nowColor + 1) / 2];
-                TD.appendChild(STONE);
             }
         });
     }
 }
 
-ARRAY[MATH_NUM / 2 - 1][MATH_NUM / 2 - 1].elm.click();
-ARRAY[MATH_NUM / 2 - 1][MATH_NUM / 2].elm.click();
-ARRAY[MATH_NUM / 2][MATH_NUM / 2].elm.click();
-ARRAY[MATH_NUM / 2][MATH_NUM / 2 - 1].elm.click();
+function reflecColor(block) {
+    block.stone.style.background = COLORS[block.num + 1];
+}
+
+ARRAY[MATH_NUM / 2][MATH_NUM / 2].num = -1;
+ARRAY[MATH_NUM / 2 - 1][MATH_NUM / 2].num = 1;
+ARRAY[MATH_NUM / 2][MATH_NUM / 2 - 1].num = 1;
+ARRAY[MATH_NUM / 2 - 1][MATH_NUM / 2 - 1].num = -1;
+
+reflecColor(ARRAY[MATH_NUM / 2][MATH_NUM / 2]);
+reflecColor(ARRAY[MATH_NUM / 2 - 1][MATH_NUM / 2]);
+reflecColor(ARRAY[MATH_NUM / 2][MATH_NUM / 2 - 1]);
+reflecColor(ARRAY[MATH_NUM / 2 - 1][MATH_NUM / 2 - 1]);
+
 
 function a(sx, sy, mx, my) {
     let x = sx + mx;
     let y = sy + my;
     let result = false;
+    if (x < 0 || MATH_NUM <= x || y < 0 || MATH_NUM <= y) {
+        while (ARRAY[y][x].num === nowColor * -1) {
+            if (x < 0 || MATH_NUM <= x || y < 0 || MATH_NUM <= y) {
+                break;
+            }
+            if (!ARRAY[y][x].num) {
+                break;
+            }
+            if (ARRAY[y + my][x + mx].num === nowColor) {
+                result = true;
+                break;
+            };
+            x += mx;
+            y += my;
+        }
+    }
+    return result;
+}
+
+function b(sx, sy, mx, my) {
+    let x = sx + mx;
+    let y = sy + my;
     while (ARRAY[y][x].num === nowColor * -1) {
-        if (x < 0 || MATH_NUM <= x || y < 0 || MATH_NUM <= y) {
-            break;
-        }
-        if (!ARRAY[y][x].num) {
-            break;
-        }
-        if (ARRAY[y + my][x + mx].num === nowColor) {
-            result = true;
-            break;
-        };
+        ARRAY[y][x].num = nowColor;
         x += mx;
         y += my;
     }
-    return result;
 }
